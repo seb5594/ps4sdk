da839db724cca004da765e7ea017093086643a30
Sebastian Schmidt <seb5594@users.noreply.github.com>
2018-03-08 23:34:19 +0100

    Update seek.c

--- base/kernel_seek_elf_address/standard/source/seek.c
+++ base/kernel_seek_elf_address/standard/source/seek.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <sys/types.h>
+
 #define PS4_KERNEL_ELF_RANGE_ADDRESS (char *)0xffffffff80000000
 enum{ PS4_KERNEL_ELF_RANGE_SIZE = 0x02000000 };
 #define PS4_KERNEL_ELF_FIXED_ADDRESS (char *)0xffffffff80700000
@@ -10,7 +11,6 @@ extern uint32_t sdkVersion;
 
 void *ps4KernelFindClosestPageStart(uint64_t addr)
 {
-
 	int remainder = addr % 0x4000;
 	if(remainder == 0)
 	{
@@ -25,51 +25,30 @@ void *ps4KernelFindClosestPageStart(uint64_t addr)
 void *ps4KernelSeekElfAddress()
 {
 	static char *addr = NULL;
+	if(addr != NULL) return addr;
 	
-	char *m;
-	int i;
 	unsigned char elfMagic[] = {0x7f, 'E', 'L', 'F', 0x02, 0x01, 0x01, 0x09, 0x00};
 	const size_t magicSize = sizeof(elfMagic) / sizeof(*elfMagic);
 	
-	if(sdkVersion>0x01760001)
+	uint64_t rip;
 	{
-		uint64_t rip;
-		{
-			unsigned long __edx;
-			unsigned long __eax;
-			__asm__ ("rdmsr" : "=d"(__edx), "=a"(__eax) : "c"(0xC0000082));
-			rip = (((uint64_t)__edx) << 32) | (uint64_t)__eax;
-		}
-	
-		char *startaddr = ps4KernelFindClosestPageStart(rip);
-	
-		for(char *m = startaddr;m > startaddr - PS4_KERNEL_ELF_RANGE_SIZE;m -= PS4_KERNEL_ELF_PAGE_SIZE)
-		{
-			for(i = 0; i < magicSize && m[i] == elfMagic[i]; ++i);
-			if(i == magicSize)
-			{
-				addr = m;
-				return m;
-			}
-		}
-		return NULL;
+		unsigned long __edx, __eax;
+		__asm__ ("rdmsr" : "=d"(__edx), "=a"(__eax) : "c"(0xC0000082));
+		rip = (uint64_t)__edx << 32 | __eax;
 	}
-	else
-	{
-		m = PS4_KERNEL_ELF_FIXED_ADDRESS;
-		for(i = 0; i < magicSize && m[i] == elfMagic[i]; ++i);
-			if(i == magicSize)
-				return m;
-			for(m = PS4_KERNEL_ELF_RANGE_ADDRESS;
-				m < PS4_KERNEL_ELF_RANGE_ADDRESS + PS4_KERNEL_ELF_RANGE_SIZE;
-				m += PS4_KERNEL_ELF_PAGE_SIZE)
-			{
-				for(i = 0; i < magicSize && m[i] == elfMagic[i]; ++i);
-				if(i == magicSize)
-					return m;
-			}
 
-		return NULL;
+	char *startaddr = xps4KernelFindClosestPageStart(rip);
+
+	for(char *m = startaddr;m > startaddr - PS4_KERNEL_ELF_RANGE_SIZE;m -= PS4_KERNEL_ELF_PAGE_SIZE)
+	{
+		for(int i = 0; i < magicSize && m[i] == elfMagic[i]; ++i)
+		{
+			if(i == magicSize - 1 && m[i] == '\0' && elfMagic[i] == '\0')
+				return addr = m;
+			else if(m[i] == '\0' ||  elfMagic[i] == '\0')
+				break;
+		}
 	}
 	
+	return NULL;
 }
